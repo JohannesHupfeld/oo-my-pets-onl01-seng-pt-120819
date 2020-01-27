5e5f9c83860b19bf647b32de493b0a0ac4421786
Johannes Hupfeld <j.hupfeld@gmail.com>
2020-01-27 21:27:25 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,22 @@
 class Cat
-  attr_accessor :mood 
-  attr_reader :name
-  
-  def initialize(name)
-    @name = name 
-    @mood = "nervous"
+
+@@all = []
+
+attr_accessor :owner, :mood
+attr_reader :cat_name
+
+def initialzize(cat_name, cat_mood = "nervous")
+  @name = name
+  @cat.owner.name = owner.name
+  @mood = mood
+
+  def can change its owner
+    @cat.owner = Owner
+   @cat.owner.name = Owner.name
   end
+  
+  def Cat.all
+    @@all
+  end
+  
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,24 @@
 class Dog
-  attr_accessor :mood
-  attr_reader :name 
   
-  def initialize(name)
-    @name = name 
-    @mood = "nervous"
+  @@all = []
+
+attr_accessor :owner, :dog_mood
+attr_reader :dog_name
+
+def initialzize(dog_name, dog_mood = "nervous")
+  @name = name
+  @mood = mood
+  @dog.owner.name = owner.name
+
+  
+  def can change its owner
+    @dog.owner = Owner
+   @dog.owner.name = Owner.name
   end
+  
+  def Dog.all
+    @@all
+  end
+  
+  
 end
